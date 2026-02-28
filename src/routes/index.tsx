9778ea586ca26ccc8c9925ad6a1d88d20218f6cd
Adam Rackis <adamrackis@hotmail.com>
2026-02-28 15:31:58 -0600

    work

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { formOptions, useForm } from "@tanstack/react-form-start";
+
+interface User {
+  firstName: string;
+  lastName: string;
+  hobbies: Array<string>;
+}
+const defaultUser: User = { firstName: "", lastName: "", hobbies: [] };
+
+const form = useForm({
+  defaultValues: defaultUser,
+  onSubmit: async ({ value }) => {
+    console.log(value);
+  },
+});
+
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
     <main className="p-8">
-      <section className="space-y-3">
-        <h1 className="text-2xl font-semibold">Hello World!</h1>
+      <section>
+        <form onSubmit={form.handleSubmit}></form>
       </section>
     </main>
   );
